04c15181244944890333e1078f1b0c80fa428775
Ivan <165202411+Molodoy-skuf-583@users.noreply.github.com>
2024-08-15 15:34:50 +0300

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,18 @@
+# call = 0
+# def count_calls():
+#
+def string_info(string):
+    string = tuple((int('Nemesis')), string_info.upper, string_info.lover)
+print(string)
+
+
+# def is_contains(string, list_to_search):
+#     string = 'new york'
+#     list_to_search = ['moscow', 'los angeles', 'peking']
+#     if string in list_to_search:
+#         print(True)
+#     else:
+#         print(False)
+#
+#
+# print(is_contains('new york'['moscow', 'los angeles', 'peking']))
